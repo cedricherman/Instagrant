595580c4103517f8e6ead02b19aeab16956745c8
Cedric Linux <herman.cedric@gmail.com>
2018-07-17 12:38:36 -0400

    added notes in src

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,5 @@
 # TO BE TRANSFERRED FROM NOTEBOOK PROTOTYPE
+#
+#######################################################
+# PLEASE SEE organize_data folder for production code
+#######################################################
